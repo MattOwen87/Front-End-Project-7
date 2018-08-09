7accc2576aa6d131b0a5cff2a63397dca13de535
Matthew Owen Haworth <matthew_haworth@hotmail.co.uk>
2018-08-09 16:25:00 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const missed = 0;
+const startButton = document.querySelector('.btn__reset');
+const startOverlay = document.getElementById('overlay');
+
+const phrases = ['mammals', 'reptiles', 'amphibians', 'birds', 'fish'];
+
+startButton.addEventListener('click', () => {
+  startOverlay.style.display = 'none';
+});
+
+function getRandomPhraseArray(arr){
+
+
+}
