3b8db4f201e912a824a5133a7c571da1eadc5efe
Matthew Owen Haworth <matthew_haworth@hotmail.co.uk>
2018-09-02 13:02:34 +0100

    update

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ const startButton = document.querySelector('.btn__reset');
 const startOverlay = document.getElementById('overlay');
 const letters = document.getElementsByClassName('letter');
 const button = document.getElementsByTagName('button');
-
 let missed = 0;
+
 const phrases = ['breaking bad', 'game of thrones', 'true detective', 'the alienist', 'bates motel'];
 
 
@@ -60,21 +60,42 @@ function checkLetter(clicked){
 
 };
 
+function checkWin(){
+
+  const correctGuess = document.getElementsByClassName('show');
+  const winOverlay = document.getElementsByClassName('win');
+  if(letters.length === correctGuess.length){
+
+    startOverlay.classList.add('win');
+    alert('Congratulations, You Win !!!!!!!');
+  } else if(missed >= 5){
+    startOverlay.classList.add('lose');
+    alert('Unlucky, You Lose Try Again');
+  }
+
+};
+
 
 qwerty.addEventListener('click', (event) => {
+
 if (event.target.tagName === 'BUTTON'){
   event.target.classList.add('chosen')
   event.target.setAttribute('disabled', true)
+  checkLetter(event.target);
+
 }
 
-const letterFound = checkLetter(event.target);
 
 
- if (letterFound === null){
+
+
+ else if (checkLetter(event.target) === null){
+
   const score = document.getElementById('scoreboard');
   const tries = document.getElementsByClassName('tries');
-  score.removeChild(tries);
   missed += 1;
+
 }
-console.log(missed);
+
+checkWin();
 });
